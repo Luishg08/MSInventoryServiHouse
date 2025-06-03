578072d3afe9f7cb52ba31ea7b725de4ebb2844c
Luis Henao <lhenao@consensussa.com>
2025-06-03 04:49:03 -0500

    Cors Policy to Gateway

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', 
-    credentials: true,
-  });
   app.setGlobalPrefix('api/inventory/')
   const config = new DocumentBuilder()
     .setTitle('MSInventory')
